c2865325cd0eb2211794306326d51d3563b94624
miru <ybmirz.freelance@gmail.com>
2024-04-30 18:07:37 +0800

    Fixed EFRQI normalisation for flat tensor input

--- experiments/circuitcomponents.py
+++ experiments/circuitcomponents.py
@@ -4,9 +4,10 @@ class CircuitComponents:
     def __init__(self):
         self.required_qubits = None
         self.name = None
-        self.available_encoders = ["Threshold_Encoder", "NEQR", "FRQI_for_2x2", "SCMFRQI_for_2x2", "EFRQI_for_2x2", "BRQI"]
+        self.available_encoders = ["Threshold_Encoder", "NEQR", "FRQI_for_2x2", "EFRQI_for_2x2", "ENEQR_for_2x2"]
         self.available_calculations = ["Randomlayer", "Nothing"]
         self.available_measurements = ["Uniform_gate_measurements", "Prob_measurement"]
 
     def circuit(self, weights):
         pass
+SCMFRQI_for_2x2
